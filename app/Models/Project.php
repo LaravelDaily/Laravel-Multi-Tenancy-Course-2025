0e3fac31ff51a385665817fefc083607a2c5be2e
PovilasKorop <povilask007@gmail.com>
2025-05-17 09:45:46 +0300

    Setting User ID: three ways

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,10 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Observers\ProjectObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+// #[ObservedBy(ProjectObserver::class)]
 class Project extends Model
 {
     protected $fillable = [
         'name',
+        'user_id',
     ];
+
+    protected static function booted()
+    {
+        static::creating(function (Project $project) {
+            $project->user_id = auth()->id();
+        });
+    }
 }
