2150082fe89f7522454fdf0b0475bd07931bede4
PovilasKorop <povilask007@gmail.com>
2025-05-18 10:25:39 +0300

    Multi-model filter with Traits

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,25 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Observers\ProjectObserver;
+use App\Traits\FilterByUser;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Builder;
 
 // #[ObservedBy(ProjectObserver::class)]
 class Project extends Model
 {
+    use FilterByUser;
+
     protected $fillable = [
         'name',
         'user_id',
     ];
 
-    protected static function booted()
-    {
-        static::creating(function (Project $project) {
-            $project->user_id = auth()->id();
-        });
+    // protected static function booted()
+    // {
+    //     static::creating(function (Project $project) {
+    //         $project->user_id = auth()->id();
+    //     });
 
-        static::addGlobalScope(function (Builder $builder) {
-            $builder->where('user_id', auth()->id());
-        });
-    }
+    //     static::addGlobalScope(function (Builder $builder) {
+    //         $builder->where('user_id', auth()->id());
+    //     });
+    // }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
+use App\Traits\FilterByUser;
 
 class Task extends Model
 {
+    // use FilterByUser;
+
     protected $fillable = [
         'name',
         'project_id',
@@ -15,5 +19,17 @@ class Task extends Model
     public function project(): BelongsTo
     {
         return $this->belongsTo(Project::class);
+    }
+
+    protected static function booted()
+    {
+        // static::creating(function (Task $task) {
+        //     $task->user_id = auth()->id();
+        // });
+
+        static::addGlobalScope(function (Builder $builder) {
+            // $builder->where('user_id', auth()->id());
+            $builder->whereRelation('project', 'user_id', auth()->id());
+        });
     }
 }
