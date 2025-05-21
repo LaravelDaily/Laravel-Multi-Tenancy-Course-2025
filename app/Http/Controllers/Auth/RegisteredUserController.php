8416fb415af1899cc0e2a21e841cfc212f1b9f1c
PovilasKorop <povilask007@gmail.com>
2025-05-21 11:40:07 +0300

    Spatie Single DB

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -41,10 +42,18 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $tenant = Tenant::create([ 
+            'name' => $request->name . ' Team',
+            'domain' => $request->subdomain . '.' . config('session.domain'),
+            'database' => $request->subdomain
+        ]);
+        $user->tenants()->attach($tenant->id); 
+
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        // return redirect(route('dashboard', absolute: false));
+        return redirect('http://' . $request->subdomain . '.' . config('session.domain') . route('dashboard', absolute: false)); 
     }
 }
